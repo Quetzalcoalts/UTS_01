7c881a139e13c72134411f0823f7001a81f8b873
Quetzalcoalts <99593775+Quetzalcoalts@users.noreply.github.com>
2022-04-07 16:16:54 +0700

    Versi 1

--- bin/c14190029_01.dart
+++ bin/c14190029_01.dart
@@ -1,3 +1,63 @@
+import 'dart:convert';
+import 'dart:io';
 void main(List<String> arguments) {
-  print('Hello world!');
+
+  print("Inputan : ");
+  String? kata = stdin.readLineSync();
+  int angka = int.parse(kata!);
+
+  print("Inputan Kata : ");
+  String? input = stdin.readLineSync();
+  int c = 0;
+  int counter = 1;
+
+  if(angka % 2 != 0){
+
+    for (int i = 1; i <= angka; i++) {
+      if (i >= 2 && i <= (angka / 2).ceil()) {
+        c = c + 1;
+      } else if (i >= (angka / 2).ceil() + 1 && i <= angka) {
+        c = c - 1;
+      }
+      if (counter > 9) {
+        counter = 1;
+      }
+
+      for (int j = 0; j < angka; j++) {
+        //atas dan bawah
+
+        if (i == 1 || i == angka) {
+          if (j == (angka - 1) / 2) {
+            stdout.write("+");
+          } else {
+            stdout.write(" ");
+          }
+        } //else if buat yang ditengah
+        else if (i == (angka + 1) / 2) {
+          if (j == 0 || j == angka-1) {
+            stdout.write("+");
+          } else {
+            stdout.write(" ");
+          }
+        } else {
+          if (j == ((angka - 1) / 2) - c || j == ((angka - 1) / 2) + c) {
+            stdout.write("+");
+          } else if (j >= ((angka - 1) / 2) - c && j <= (angka - 1) / 2 ||
+              j <= ((angka - 1) / 2) + c && j >= (angka - 1) / 2) {
+            stdout.write(" ");
+          } else {
+            stdout.write(" ");
+          }
+        }
+      }
+      print("");
+      counter++;
+    }
+    
+  }
+  else{
+    stdout.write("$input");
+    print("");
+    stdout.write("$angka");
+  }
 }
